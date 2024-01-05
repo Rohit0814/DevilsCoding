5dc908a38c88cd7931e868d276f4803629ea39cd
rohitsingh0814 <rohitkumar.singh2022@vitstudent.ac.in>
2024-01-06 00:56:31 +0530

    footer complete design

--- resources/views/Comman/Footer.blade.php
+++ resources/views/Comman/Footer.blade.php
@@ -1,4 +1,50 @@
-<h1>Footer</h1>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<footer class="mt-10 w-full bottom-0 pt-8" style="-webkit-box-shadow: 0px 2px 8px 2px rgba(0,0,0,0.75); -moz-box-shadow: 0px 2px 8px 2px rgba(0,0,0,0.75); box-shadow: 0px 2px 8px 2px rgba(0,0,0,0.75);">
+    <div class="container mx-auto pb-8 flex flex-wrap sm:pl-40 sm:pr-40 md:pl-10 md:pr-10 pl-0 pr-0">
+      
+      <!-- First Section -->
+      <div class="w-full md:w-1/3 px-9 mb-4 md:mb-0 border-r-2">
+        <h2 class="text-2xl font-bold mb-4">Devils Coding</h2>
+        <p>Welcome to Technology Arena. Everyone has power to change world using moden technologies</p>
+      </div>
+      
+      <!-- Second Section -->
+      <div class="w-full md:w-1/3 px-9 mb-4 md:mb-0 border-r-2">
+        <h2 class="text-2xl font-bold mb-4">Contact Us</h2>
+        <p>Email: dc@devilscoding.tech</p>
+        <p>Phone: 6299827726</p>
+        <p>Address: Ranchi, Jharkhand</p>
+      </div>
+      
+      <!-- Third Section -->
+      <div class="w-full md:w-1/3 px-9">
+        <h2 class="text-2xl font-bold mb-4">Follow Us</h2>
+        <div class="flex space-x-5">
+          <a href="#" class="text-gray-600 text-2xl hover:text-indigo-600 transition duration-300">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
+          <a href="#" class="text-gray-600 text-2xl hover:text-indigo-600 transition duration-300">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+          <a href="#" class="text-gray-600 text-2xl hover:text-indigo-600 transition duration-300">
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+          <a href="#" class="text-gray-600 text-2xl hover:text-indigo-600 transition duration-300">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
+
+    </div>
+    <div class="bg-black w-full text-white text-center text-sm pt-3 pb-3 font-bold">
+        © 2020 Your Company, Inc. All rights reserved.
+    </div>
+  </footer>
+
+</div>
 
 </body>
 </html>
